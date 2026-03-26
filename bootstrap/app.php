d249acd22e7de6a501efccdc14df8ad952874359
Andrea D'Atri <andrea.datri80@gmail.com>
2026-03-26 08:00:07 +0100

    Remove controlled Nightwatch error endpoint

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,16 +1,12 @@
 <?php
 
-use App\Exceptions\ControlledNightwatchException;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\EnsureUserIsAdmin;
 use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
-use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -33,15 +29,4 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->report(function (ControlledNightwatchException $exception): void {
-            Log::error('Controlled Nightwatch exception captured', [
-                'message' => $exception->getMessage(),
-                'class' => $exception::class,
-            ]);
-        });
-
-        $exceptions->render(function (ControlledNightwatchException $exception, Request $request): Response {
-            return response($exception->getMessage(), 500)
-                ->header('X-Nightwatch-Controlled-Error', '1');
-        });
     })->create();
